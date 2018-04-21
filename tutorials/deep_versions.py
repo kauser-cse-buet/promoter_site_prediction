f2d2eee0d88677c37a4f146dec07945db72dbc53
Md Kauser Ahmmed <anik2137@gmail.com>
2018-04-20 22:20:43 -0500

    update

---
+++ tutorials/deep_versions.py
@@ -0,0 +1,12 @@
+# how to setup deep nn environment 
+# https://machinelearningmastery.com/setup-python-environment-machine-learning-deep-learning-anaconda/
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
